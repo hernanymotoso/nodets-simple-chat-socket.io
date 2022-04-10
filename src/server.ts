69b4783ec4ed7e1aa522c871820e8c4d2f983a92
Hernany Motoso <contatohernany@gmail.com>
2022-04-10 10:11:06 -0300

    feat: socket connection creation

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import path from 'path';
 import http from 'http';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 
 const server = http.createServer(app);
-
+const io = new Server(server);
 // app.get('/', (request, response) => response.json({ message: 'Hello world' }));
+
+io.on('connection', (socket: Socket) => {
+  console.log('Connectado novo');
+});
 
 server.listen(3333, () => {
   console.log('Server started on port 3333 !');
