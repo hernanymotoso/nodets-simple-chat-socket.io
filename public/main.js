9aa13b3c32ddffa2c49ab8354b74a4515a772bef
Hernany Motoso <contatohernany@gmail.com>
2022-04-10 10:37:32 -0300

    feat: chat enter creation

--- public/main.js
+++ public/main.js
@@ -1,1 +1,28 @@
 const socket = io();
+
+socket.on('connect', () => {
+  console.log('connect client');
+});
+
+let userName = '';
+let userList = [];
+
+let loginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chatPage');
+
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector('#chatTextInput');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+
+loginInput.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13 ){
+    let name = loginInput.value.trim();
+    if(name !== '') {
+      userName = name;
+      document.title = 'Chat ('+userName+')';
+    }
+  }
+})
